6992175cc602efb8feaf9392745b115a81a28e85
Younes Bouhadjar <younes.bouhadjy@gmail.com>
2018-04-11 18:30:10 -0700

    change jumping mechanism

--- ntm/ntm_cell.py
+++ ntm/ntm_cell.py
@@ -22,7 +22,7 @@ class NTMCell(nn.Module):
         super(NTMCell, self).__init__()
         tm_state_in = tm_state_units + tm_in_dim
         self.tm_i2w_0 = nn.Linear(tm_state_in, num_heads)
-        self.tm_i2w_dynamic = nn.Linear(tm_state_in, 2)
+        self.tm_i2w_dynamic = nn.Linear(tm_state_in, 3)
 
         # build the interface and controller
         self.interface = Interface(num_heads, is_cam, num_shift, M)
@@ -48,13 +48,14 @@ class NTMCell(nn.Module):
 
         wt_address_dynamic = (1 - f) * wt + f * wt_address_dynamic
 
-        wt = (1 - h[:,0]) * (1 - h[:,1]) * wt_address_0 \
-                  + (1 - h[:,0]) * h[:,1] * wt \
-                  + h[:, 0] * (1 - h[:, 1]) * wt_address_dynamic
+        # wt = (1 - h[:,0]) * (1 - h[:,1]) * wt_address_0 \
+        #           + (1 - h[:,0]) * h[:,1] * wt \
+        #           + h[:, 0] * (1 - h[:, 1]) * wt_address_dynamic
 
-        # wt = h[:,0]* wt_address_0 \
-        #    + h[:,1] * wt \
-        #    + h[:,2] * wt_address_dynamic
+        h = normalize(h)
+        wt = h[:,0]* wt_address_0 \
+            + h[:,1] * wt \
+            + h[:,2] * wt_address_dynamic
 
 
         # step1: read from memory using attention
