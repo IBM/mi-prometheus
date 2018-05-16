4e30f86a6925f1e6d5ef1b2faaec897b87e7d562
Alexis Asseman <alexis.asseman@ibm.com>
2018-05-15 21:31:12 -0700

    WIP on batch_test.py

--- batch_test.py
+++ batch_test.py
@@ -7,13 +7,14 @@ It will run as many concurrent jobs as possible.
 
 import os
 import sys
-import yaml
-from random import randrange
-from itertools import repeat
-from tempfile import NamedTemporaryFile
 from multiprocessing.pool import ThreadPool
 import subprocess
 import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')  # Headless backend for matplotlib
+import matplotlib.pyplot as plt
+
 
 def main():
     batch_file = sys.argv[1]
@@ -22,8 +23,8 @@ def main():
     # Load the list of yaml files to run
     with open(batch_file, 'r') as f:
         directory_checkpoints = [l.strip() for l in f.readlines()]
-        for folderename in directory_checkpoints:
-            assert os.path.isdir(folderename), folderename + " is not a file"
+        for foldername in directory_checkpoints:
+            assert os.path.isdir(foldername), foldername + " is not a file"
 
     experiments_list = []
     for elem in directory_checkpoints:
@@ -33,9 +34,9 @@ def main():
             checkpoints = os.path.join(elem, sub)
             experiments_list.append(checkpoints)
 
-    for elem in experiments_list:
-        if os.path.isfile(elem + '/validation.csv'):
-            validation_losses = np.loadtxt(elem + '/validation.csv', delimiter=', ', skiprows=1)
+    # Keep only the folders that contain validation.csv and training.csv
+    experiments_list = [elem for elem in experiments_list
+                        if os.path.isfile(elem + '/validation.csv') and os.path.isfile(elem + '/training.csv')]
 
     # Run in as many threads as there are CPUs available to the script
     with ThreadPool(processes=len(os.sched_getaffinity(0))) as pool:
@@ -43,14 +44,22 @@ def main():
 
 
 def run_experiment(path: str):
+    val_episode, val_accuracy, val_loss, val_length = np.loadtxt(path + '/validation.csv', delimiter=', ', skiprows=1)
+    train_episode, train_accuracy, train_loss, train_length = np.loadtxt(path + '/training.csv', delimiter=', ', skiprows=1)
 
-        command_str = "cuda-gpupick -n0 python3 test.py -i {0} ".format(path).split()
+    # Save plot of losses to png file
+    plt.semilogy(val_episode, val_loss, label='validation loss')
+    plt.semilogy(train_episode, train_loss, label='training loss')
+    plt.savefig(path + '/loss.png')
+    plt.close()
 
-        with open(os.devnull, 'w') as devnull:
-            result = subprocess.run(command_str, stdout=devnull)
+    command_str = "cuda-gpupick -n0 python3 test.py -i {0} ".format(path).split()
 
-        if result.returncode != 0:
-            print("Training exited with code:", result.returncode)
+    with open(os.devnull, 'w') as devnull:
+        result = subprocess.run(command_str, stdout=devnull)
+
+    if result.returncode != 0:
+        print("Training exited with code:", result.returncode)
 
 
 if __name__ == '__main__':
