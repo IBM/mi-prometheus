2f3dbe882597d98a11dfb9946e7bd57e34feb6fb
Ryan McAvoy <mcavoy@us.ibm.com>
2018-05-16 14:16:03 -0700

    Feat fixed model saving (#116)
    * Fixed memory usage problem with torch.no_grad
    
    * stopped it from saving everytime it improved and made so it saved at regular intervals

--- train.py
+++ train.py
@@ -392,9 +392,9 @@ if __name__ == '__main__':
                 break
 
         # 5. Validation. check if new loss is smaller than the best loss, save the model in this case
-        if loss < best_loss or (episode % validation_frequency) == 0:
+        if (episode % validation_frequency) == 0:
 
-            improved = False
+            improved = True
             if loss < best_loss:
                 best_loss = loss
                 improved = True
