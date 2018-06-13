624f65ce93ff92dfd20ffa4299a2a9de80122046
Younes Bouhadjar <younes.bouhadjy@gmail.com>
2018-06-13 15:14:17 -0700

    add permuted mnist

---
+++ problems/sequence/seq_to_class/SeqMNIST/permuted_sequential_row_mnist.py
@@ -0,0 +1,77 @@
+import torch
+from sequential_vision_problem import SequentialVisionProblem
+from problems.sequence.sequential_problem import MaskAuxTuple
+from problems.problem import DataTuple
+from torchvision import datasets, transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+
+
+class PermutedSequentialRowMnist(SequentialVisionProblem):
+    """
+    Class generating sequences sequential mnist
+    """
+
+    def __init__(self, params):
+        """ Initialize. """
+        # Call base class constructors.
+        super(PermutedSequentialRowMnist, self).__init__(params)
+
+        # Retrieve parameters from the dictionary.
+        self.batch_size = params['batch_size']
+        self.start_index = params['start_index']
+        self.stop_index = params['stop_index']
+        self.num_rows = 28
+        self.num_columns = 28
+        self.use_train_data = params['use_train_data']
+        self.datasets_folder = params['mnist_folder']
+
+    def generate_batch(self):
+        # define transforms
+        pixel_permutation = torch.randperm(28)
+        train_transform = transforms.Compose([
+            transforms.ToTensor(), transforms.Lambda(lambda x: x[:, pixel_permutation])])
+
+        # load the datasets
+        train_datasets = datasets.MNIST(self.datasets_folder, train=True, download=True,
+                                     transform=train_transform)
+        # set split
+        num_train = len(train_datasets)
+        indices = list(range(num_train))
+
+        idx = indices[self.start_index: self.stop_index]
+        sampler = SubsetRandomSampler(idx)
+
+        # loader
+        train_loader = torch.utils.data.DataLoader(train_datasets, batch_size=self.batch_size,
+                                                   sampler=sampler)
+        # create an iterator
+        train_loader = iter(train_loader)
+
+        # create mask
+        mask = torch.zeros(self.num_rows)
+        mask[-1] = 1
+
+        # train_loader a generator: (data, label)
+        (data, label) = next(train_loader)
+
+        # Return DataTuple(!) and an empty (aux) tuple.
+        return DataTuple(data,label), MaskAuxTuple(mask.type(torch.uint8))
+
+if __name__ == "__main__":
+    """ Tests sequence generator - generates and displays a random sample"""
+
+    # "Loaded parameters".
+    params = {'batch_size': 1, 'start_index': 0, 'stop_index': 54999, 'use_train_data': True, 'mnist_folder': '~/data/mnist'}
+    # Create problem object.
+    problem = PermutedSequentialRowMnist(params)
+    # Get generator
+    generator = problem.return_generator()
+    # Get batch.
+    num_rows = 28
+    num_columns = 28
+    sample = 0
+    data_tuple, _ = next(generator)
+    x, y = data_tuple
+
+    # Display single sample (0) from batch.
+    problem.show_sample(x[sample, 0], y)
