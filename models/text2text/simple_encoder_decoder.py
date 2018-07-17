5d173643d65b9e211aad510c7ed4d89851c290f5
Vincent Marois <vmarois@us.ibm.com>
2018-07-16 17:01:24 -0700

    Added the license headers.

--- models/text2text/simple_encoder_decoder.py
+++ models/text2text/simple_encoder_decoder.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Implementation of an Encoder-Decoder network for text2text problems (e.g. translation)"""
+#
+# Copyright (C) IBM Corporation 2018
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""simple_encoder_decoder.py: Implementation of an Encoder-Decoder network for text2text problems (e.g. translation)
+    Inspiration taken from the corresponding Pytorch tutorial.
+    See https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html """
 __author__ = "Vincent Marois "
 
 import torch
