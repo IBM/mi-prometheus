42d4fe2f7dd8a40d70ab48cd7169a0eacfbb2104
Ryan McAvoy <rlmcavoy@wayneslab-galileo.almaden.ibm.com>
2018-07-16 17:12:24 -0700

    added license headers to the DNC model

--- models/dnc/temporal_linkage.py
+++ models/dnc/temporal_linkage.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) IBM Corporation 2018
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""temporal_linkage.py: Class governing the main read mechanism of the DNC.
+                    This is a Pytorch conversion of the TemporalLinkage class from Deepmind's Tensorflow implementation 
+                    of the DNC (Copyright 2017 Google Inc.) https://github.com/deepmind/dnc/blob/master/addressing.py
+"""
+
+__author__ = " Ryan L. McAvoy"
+
 import torch
 import collections 
 from misc.app_state import AppState
