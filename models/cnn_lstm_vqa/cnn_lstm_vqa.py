2c72ac1cdd901e566e4053c8e8f962ec0989edcf
Younes Bouhadjar <younes.bouhadjy@gmail.com>
2018-07-16 21:46:06 -0700

    add License to VQA baselines

--- models/cnn_lstm_vqa/cnn_lstm_vqa.py
+++ models/cnn_lstm_vqa/cnn_lstm_vqa.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) IBM Corporation 2018
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""cnn_lstm_vqa.py: Simple design to solve VQA problem using a CNN for image encoding and LSTM for question encoding
+https://arxiv.org/pdf/1512.02167.pdf """
+__author__ = "Younes Bouhadjar"
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
--- models/stacked_attention_vqa/attention.py
+++ models/stacked_attention_vqa/attention.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) IBM Corporation 2018
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""attention.py: implement different attention model: for now just the stacked attention exists https://arxiv.org/abs/1511.02274
+ for future co-attention needs to be implemented https://arxiv.org/abs/1707.04968 """
+__author__ = "Younes Bouhadjar"
+
 import torch.nn as nn
 import torch.nn.functional as F
 
