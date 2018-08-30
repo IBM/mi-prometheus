Fixed commenting, removed dead code, allowed for masking to be turned off

problems/problem_utils.py
@@ -8,17 +8,22 @@ import torch
 from misc.app_state import AppState
 
 class MaskedCrossEntropyLoss(nn.Module):
-    #Calculates the cross entropy for batches with different numbers of outputs for the samples
+    """Calculates the cross entropy for batches with different numbers of outputs per
+    """
 
-    def __init__(self, weight=None, ignore_index=-100, use_mask = True):
+    def __init__(self, weight=None, ignore_index=-100):
         super(MaskedCrossEntropyLoss, self).__init__()
         self.loss_function = nn.CrossEntropyLoss(reduce = False)
-        self.loss_function2 = nn.CrossEntropyLoss()
-        self.use_mask = use_mask
-        #self.loss_function2 = nn.CrossEntropyLoss()
         #for pytorch 4.1
         #self.loss_function = nn.CrossEntropyLoss(reduction = "none")
     def forward(self, logits, targets, mask):
+        """ Calculates loss accounting for different numbers of output per sample
+
+        :param logits: Logits being output by the model. [batch, classes, sequence]
+        :param targets: LongTensor targets [batch, sequence]
+        :param mask: ByteTensor mask [batch, sequence]
+        """
+
         loss_per_element = self.loss_function(logits, targets)
 
         #Have to convert the mask to floats to multiply by the loss
@@ -30,32 +35,20 @@ class MaskedCrossEntropyLoss(nn.Module):
         #obtain the number of non-zero elements in the mask.
         # nonzero() returns the indices so you have to divide by the number of dimensions
         size= mask.nonzero().numel()/len(mask.shape)
-	#size= masked_loss_per.nonzero().numel()/len(masked_loss_per.shape) 
 
         loss = torch.sum(masked_loss_per)/size
-
-        masque = mask[0].type(torch.cuda.ByteTensor)
-        a = logits[:,:, masque]
-        b= targets[:, masque]
-        loss2 = self.loss_function2(a,b)
-        print(loss.item(), loss2.item(), loss.item()-loss2.item())
         return loss
 
     def masked_accuracy(self, logits, targets, mask):
         """ Calculates accuracy equal to mean number of correct predictions in a given batch.
-        WARNING: Applies mask (from aux_tuple) to logits!
 
-        :param logits: Logits being output of the model.
-        :param targets: Data tuple containing inputs and targets.
-        :param mask: Auxiliary tuple containing mask.
+        :param logits: Logits being output by the model. [batch, classes, sequence]
+        :param targets: LongTensor targets [batch, sequence]
+        :param mask: ByteTensor mask [batch, sequence]
+
         """
-        # Mask logits (ONLY LOGITS!)
-        #masked_logits = logits[:, aux_tuple.mask, :][:, 0, :]
-
         # Get the index of the max log-probability.
         pred = logits.max(1, keepdim=True)[1]
-        #print(pred.shape)
-        #print(targets.shape)
         correct_per = pred.eq(targets.view_as(pred))
 
         if len(mask.shape) < len(targets.shape):
@@ -71,15 +64,23 @@ class MaskedCrossEntropyLoss(nn.Module):
         return accuracy
 
 class MaskedBCEWithLogitsLoss(nn.Module):
-    #Calculates the binary cross entropy for batches with different numbers of outputs for the samples
+    """Calculates the binary cross entropy for batches with different numbers of outputs for the samples
+    """
 
     def __init__(self, weight=None):
         super(MaskedBCEWithLogitsLoss, self).__init__()
         self.loss_function = nn.BCEWithLogitsLoss(reduce = False)
-        self.loss_function2 = nn.BCEWithLogitsLoss()
         #for pytorch 4.1
         #self.loss_function = nn.BCELossWithLogits(reduction = "none")
     def forward(self, logits, targets, mask):
+        """ Calculates loss accounting for different numbers of output per sample
+
+        :param logits: Logits being output by the model. [batch, sequence, element_size]
+        :param targets: LongTensor targets [batch, sequence, element_size]
+        :param mask: ByteTensor mask [batch, sequence]
+        """
+
+
         loss_per_element = self.loss_function(logits, targets)
  
         #The mask lacks the last dimension of the targets so needs to be scaled up
@@ -96,22 +97,16 @@ class MaskedBCEWithLogitsLoss(nn.Module):
         size = mask.nonzero().numel()/len(mask.shape)*logits.shape[-1]
 
         loss = torch.sum(masked_loss_per)/size
-
-        masque = mask[0].type(torch.cuda.ByteTensor)
-        a = logits[:, masque,:]
-        b= targets[:, masque]
-        loss2 = self.loss_function2(a,b)
-        print(loss.item(), loss2.item(), loss.item()-loss2.item())
-
+ 
         return loss
 
     def masked_accuracy(self, logits, targets, mask):
         """ Calculate accuracy equal to mean difference between outputs and targets.
         WARNING: Applies mask (from aux_tuple) to both logits and targets!
 
-        :param logits: Logits being output of the model.
-        :param targets: Data tuple containing inputs and targets.
-        :param mask: Auxiliary tuple containing mask.
+        :param logits: Logits being output by the model. [batch, sequence, element_size]
+        :param targets: LongTensor targets [batch, sequence, element_size]
+        :param mask: ByteTensor mask [batch, sequence]
         """
 
         acc_per =  1 - torch.abs(torch.round(F.sigmoid(logits)) - targets)

problems/seq_to_seq/algorithmic/algorithmic_seq_to_seq_problem.py
@@ -40,8 +40,10 @@ class AlgorithmicSeqToSeqProblem(SeqToSeqProblem):
         super(AlgorithmicSeqToSeqProblem, self).__init__(params)
 
         # Set default loss function - cross entropy.
-        #self.loss_function = nn.BCEWithLogitsLoss()
-        self.loss_function = MaskedBCEWithLogitsLoss()
+        if self.use_mask:
+            self.loss_function = MaskedBCEWithLogitsLoss()
+        else:
+            self.loss_function = nn.BCEWithLogitsLoss()
 
         # Extract "standard" list of parameters for algorithmic tasks.
         self.batch_size = params['batch_size']