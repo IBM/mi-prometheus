2b43cdde2da6025a61901acc10c578634f1c9a63
Vincent Albouy <vincent.albouy@ibm.com>
2018-09-05 15:31:18 -0700

    summary commit

--- trainer.py
+++ trainer.py
@@ -219,6 +219,39 @@ if __name__ == '__main__':
     model = ModelFactory.build_model(param_interface['model'])
     model.cuda() if app_state.use_CUDA else None
 
+    from torch.nn.modules.module import _addindent
+    import torch
+    import numpy as np
+
+
+    def torch_summarize(model, show_weights=True, show_parameters=True):
+        """Summarizes torch model by showing trainable parameters and weights."""
+        tmpstr = model.__class__.__name__ + ' (\n'
+        for key, module in model._modules.items():
+            # if it contains layers let call it recursively to get params and weights
+            if type(module) in [
+                torch.nn.modules.container.Container,
+                torch.nn.modules.container.Sequential
+            ]:
+                modstr = torch_summarize(module)
+            else:
+                modstr = module.__repr__()
+            modstr = _addindent(modstr, 2)
+
+            params = sum([np.prod(p.size()) for p in module.parameters()])
+            weights = tuple([tuple(p.size()) for p in module.parameters()])
+
+            tmpstr += '  (' + key + '): ' + modstr
+            if show_weights:
+                tmpstr += ', weights={}'.format(weights)
+            if show_parameters:
+                tmpstr += ', parameters={}'.format(params)
+            tmpstr += '\n'
+
+        tmpstr = tmpstr + ')'
+        return tmpstr
+
+
     # Build problem for the training
     problem = ProblemFactory.build_problem(param_interface['training']['problem'])
 
