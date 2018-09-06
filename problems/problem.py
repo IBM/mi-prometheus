f683359950765af90295873ffe67886f5b22d567
Vincent Marois <vmarois@us.ibm.com>
2018-09-06 16:22:02 -0700

    Started problems standardization.

--- problems/problem.py
+++ problems/problem.py
@@ -5,7 +5,9 @@ __author__      = "Tomasz Kornuta"
 
 
 import collections
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod
+from torch.utils.data import Dataset
+import torch
 
 _DataTuple = collections.namedtuple('DataTuple', ('inputs', 'targets'))
 class DataTuple(_DataTuple):
@@ -22,7 +24,6 @@ class MaskAuxTuple(_MaskAuxTuple):
     __slots__ = ()
 
 
-
 _LabelAuxTuple = collections.namedtuple('LabelAuxTuple', ('label'))
 class LabelAuxTuple(_LabelAuxTuple):
     """
@@ -31,8 +32,7 @@ class LabelAuxTuple(_LabelAuxTuple):
     __slots__ = ()
 
 
-
-class Problem(metaclass=ABCMeta):
+class Problem(Dataset):
     """ Class representing base class for all Problems.
     """
 
@@ -45,35 +45,58 @@ class Problem(metaclass=ABCMeta):
         # Set default loss function.
         self.loss_function = None
 
+        # Set default collate function
+        self.collate_fn = torch.utils.data.default_collate
+
         # Store pointer to params.
         self.params = params
 
+        # size of the dataset
+        self.length = None
 
     def set_loss_function(self, loss_function):
         """ Sets loss function.
 
-        :param criterion: Loss function (e.g. nn.CrossEntropyLoss()) that will be set as optimization criterion.
+        :param loss_function: Loss function (e.g. nn.CrossEntropyLoss()) that will be set as optimization criterion.
         """
         self.loss_function = loss_function
 
+    def set_collate_function(self, collate_function):
+        """ Sets collate function.
+
+        :param collate_function: Loss function that will be used to merge a list of individuals samples into a batch.
+        """
+        self.collate_function = collate_function
 
     @abstractmethod
-    def generate_batch(self):
+    def __getitem__(self, item):
         """
-        Generates batch of sequences of given length. 
-        Abstract - to be defined in derived classes. 
+        Getter that returns an invidual sample from the problem's associated dataset (that can be generated on-the-fly,
+        or retrieved from disk).
+
+        To be redefined in derived classes.
+        :param item: index of the sample to return
+        :return: sample.
         """
 
-    def return_generator(self):
+    def __len__(self):
         """
-        Returns a generator yielding a batch  of size [BATCH_SIZE, 2*SEQ_LENGTH+2, CONTROL_BITS+DATA_BITS].
-        Additional elements of sequence are  start and stop control markers, stored in additional bits.
-       
-        : returns: A tuple: input with shape [BATCH_SIZE, 2*SEQ_LENGTH+2, CONTROL_BITS+DATA_BITS], output 
+        :return: The size of the dataset.
+
         """
-        # Create "generator".
-        while True:
-            yield self.generate_batch()
+        return self.length
+
+    @abstractmethod
+    def collate_fn(self, batch):
+        """
+        Generates a batch of samples from a list of individuals samples retrieved by __getitem__.
+        The default collate_fn is torch.utils.data.default_collate.
+
+        Abstract - to be defined in derived classes.
+        :param batch: iterator of samples to merge into one batch.
+
+        ;:return created batch. #TODO: Should it be a DataTuple ?
+        """
 
     def evaluate_loss(self, data_tuple, logits, _):
         """ Calculates loss between the predictions/logits and targets (from data_tuple) using the selected loss function.
@@ -111,6 +134,41 @@ class Problem(metaclass=ABCMeta):
         """
         pass
 
+    def get_epoch_size(self):
+        """
+        Compute the number of iterations ('episodes') to do given the size of the dataset and the batch size.
+        Note: we are compting the last batch, even though it might be smaller than the other ones if the size of the
+        dataset is not divisible by the batch size. -> Corresponds to drop_last=False in DataLoader().
+        :return: Number of iterations to perform to go though the entire dataset once.
+        """
+        return self.length // self.params['batch_size'] + 1
+
+    def initialize_epoch(self):
+        """
+        Function called to initialize a new epoch.
+        Used to reset statistics collector over one epoch, e.g.:
+            - Average accuracy over the epoch
+            - Time taken for the epoch and average per batch
+            - etc...
+        """
+
+        # TODO
+        pass
+
+    def finalize_epoch(self):
+        """
+        Function called at the end of an epoch to execute a few tasks, e.g.:
+            - Compute the mean accuracy over the epoch
+            - Get the time taken for the epoch and per batch
+            - etc...
+
+        It should pass those values to the Logger
+        """
+        pass
+
+
+
+'''
     def turn_on_cuda(self, data_tuple, aux_tuple):
         """ Enables computations on GPU - copies the input and target matrices (from DataTuple) to GPU.
         This method has to be overwritten in derived class if one decides to copy other matrices as well.
@@ -162,3 +220,4 @@ class Problem(metaclass=ABCMeta):
         :returns: True informing that CL wasn't active at all (i.e. is finished).
         """
         return True
+'''
