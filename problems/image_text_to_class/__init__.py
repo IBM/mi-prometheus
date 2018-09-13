894802a1f264936b41d647180bbd2a59fa913d42
Vincent Marois <vmarois@us.ibm.com>
2018-09-13 12:01:22 -0700

    Added back CLEVR in import lines.

--- problems/image_text_to_class/__init__.py
+++ problems/image_text_to_class/__init__.py
@@ -1,5 +1,5 @@
-#from .clevr import CLEVR
-#from .generate_feature_maps import GenerateFeatureMaps
+from .clevr import CLEVR
+from .generate_feature_maps import GenerateFeatureMaps
 from .image_text_to_class_problem import ObjectRepresentation, ImageTextToClassProblem
 #from .sort_of_clevr import SortOfCLEVR
 #from .shape_color_query import ShapeColorQuery
@@ -7,4 +7,4 @@ from .image_text_to_class_problem import ObjectRepresentation, ImageTextToClassP
 #__all__ = ['CLEVR', 'GenerateFeatureMaps', 'ImageTextDict', 'SceneDescriptionTuple',
 #           'ObjectRepresentation', 'ImageTextToClassProblem', 'SortOfCLEVR', 'ShapeColorQuery']
 
-__all__ = ['ObjectRepresentation', 'ImageTextToClassProblem']
+__all__ = ['CLEVR', 'GenerateFeatureMaps', 'ObjectRepresentation', 'ImageTextToClassProblem']
