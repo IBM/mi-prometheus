4c95ddf459b64e7bb8f778397fbc4e120e0698c7
Vincent Marois <vmarois@us.ibm.com>
2018-10-01 13:43:11 -0700

    Refactored basic workers as classes. Also fixed the Tester.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .models import *
 from .problems import *
 from .utils import *
+from .workers import *
 
 from .trainer import validation
---
+++ workers/__init__.py
@@ -0,0 +1,3 @@
+from .base_worker import add_arguments, BaseWorker
+from .base_trainer import add_arguments, BaseTrainer
+from .base_tester import add_arguments, Tester
