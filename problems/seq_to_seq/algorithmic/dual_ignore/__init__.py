2418a8037f197fccc78901b786c39bfb95f54a62
Tomasz Kornuta <tkornut@us.ibm.com>
2018-10-18 17:31:16 -0700

    Reorganization of algorithmic tasks

---
+++ problems/seq_to_seq/algorithmic/dual_ignore/__init__.py
@@ -0,0 +1,9 @@
+from .interruption_not import InterruptionNot
+from .interruption_reverse_recall import InterruptionReverseRecall
+from .interruption_swap_recall import InterruptionSwapRecall
+
+__all__ = [
+    'InterruptionNot',
+    'InterruptionReverseRecall',
+    'InterruptionSwapRecall'
+    ]
