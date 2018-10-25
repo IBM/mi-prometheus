1f1f47d300e0a9f34cc9fc2e4f91b471541e98ff
tkornut <tkornut@us.ibm.com>
2018-10-25 16:05:48 -0700

    Added sampler factory to trainers, index selection not used/fully functional yet

---
+++ miprometheus/utils/sampler_factory.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) IBM Corporation 2018
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+sampler_factory.py: Factory building samplers used by PyTorch's DataLoaders.
+
+"""
+__author__ = "Tomasz Kornuta"
+import logging
+import inspect
+import torch.utils.data.sampler
+
+
+class SamplerFactory(object):
+    """
+    Class returning sampler depending on the name provided in the \
+    list of parameters.
+    """
+
+    @staticmethod
+    def build(problem, params):
+        """
+        Static method returning particular sampler, depending on the name \
+        provided in the list of parameters.
+
+        :param problem: Instance of an object derived from the Problem class.
+        :type problem: ``problems.Problem``
+
+        :param params: Parameters used to instantiate the sampler.
+        :type params: ``utils.param_interface.ParamInterface``
+
+        ..note::
+
+            ``params`` should contains the exact (case-sensitive) class name of the sampler to instantiate.
+
+        :return: Instance of a given sampler or None if section not present (None) or coudn't build the sampler.
+        """
+        logging.basicConfig(level=logging.INFO)
+        logger = logging.getLogger('SamplerFactory')
+
+        # Check if sampler is required, i.e. 'sampler' section is empty.
+        if not params:
+            return None
+
+        try: 
+            # Check presence of the name attribute.
+            if 'name' not in params:
+                raise Exception("Sampler configuration section does not contain the key 'name'")
+
+            # Get the class name.
+            name = params['name']
+
+            # Verify that the specified class is in the controller package.
+            if name not in dir(torch.utils.data.sampler):
+                raise Exception("Could not find the specified class '{}' in the controllers package".format(name))
+
+            # Get the actual class.
+            sampler_class = getattr(torch.utils.data.sampler, name)
+
+
+            # Ok, proceed.
+            logger.info('Loading the {} controller from {}'.format(name, sampler_class.__module__))
+
+            # Handle "special" case.
+            if name == 'SubsetRandomSampler':
+                indices  = params['indices']
+                # Check if indices are ok.
+                sampler = sampler_class(indices)
+            else:
+                # Create "regular" sampler.
+                sampler = sampler_class(problem)
+
+            # Return sampler.
+            return sampler
+        except Exception as e:
+            logger.error(e)
+            logger.warning("Using default sampling without sampler.")
+            return None
+
+
+if __name__ == "__main__":
+    """
+    Tests the factory.
+    """
+    from miprometheus.utils.param_interface import ParamInterface
+
+    # Problem.
+    class TestProblem(object):
+        def __len__(self):
+            return 100
+    # All samplers operate on TestProblem only, whereas SubsetRandomSampler additinally accepts 'incidces' with three options.
+    # Option 1: range.
+    indices = range(150)
+    # Option 2: range as str.
+    range_str = '[0 50]'
+    # Option 3: name of the file containing indices.
+    filename = "indices.txt"
+
+    params = ParamInterface()
+    params.add_default_params({'name': 'SubsetRandomSampler',
+                                'indices': indices})
+
+    sampler = SamplerFactory.build(TestProblem(), params)
+    print(type(sampler))
+
+    for i, index in enumerate(sampler):
+        print('{}: index {}'.format(i, index))
+        pass
+
