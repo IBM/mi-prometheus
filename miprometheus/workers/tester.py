3966afe9d529b39b1b094bd4fafcd49d5b8dd8f5
Tomasz Kornuta <43558388+tkornut@users.noreply.github.com>
2018-10-31 11:25:58 -0700

    Update tester.py
    fixed typo

--- miprometheus/workers/tester.py
+++ miprometheus/workers/tester.py
@@ -219,7 +219,7 @@ class Tester(Worker):
             exit(-5)
         except Exception as e:
             self.logger.error(e)
-            # Exit by following the logic: if user wanted to load the model but failer, then continuing the experiment makes no sense.
+            # Exit by following the logic: if user wanted to load the model but failed, then continuing the experiment makes no sense.
             exit(-6)
 
         # Turn on evaluation mode.
