5b41532c00a1821804d282d07f412a8ca3373e50
Vincent Marois <vmarois@us.ibm.com>
2018-11-12 08:39:29 -0800

    Made some methods static + polishing.

--- miprometheus/grid_workers/grid_analyzer.py
+++ miprometheus/grid_workers/grid_analyzer.py
@@ -28,8 +28,8 @@ __author__ = "Tomasz Kornuta & Vincent Marois"
 import os
 import csv
 import yaml
-import logging
 import torch
+import logging
 from datetime import datetime
 
 from miprometheus.grid_workers.grid_worker import GridWorker
@@ -37,58 +37,87 @@ from miprometheus.grid_workers.grid_worker import GridWorker
 
 class GridAnalyzer(GridWorker):
     """
-    Implementation of the Grid Analyzer. Post-processes the test results of a grid of experiments and gather them in\
-     a csv file.
+    Implementation of the :py:class:`miprometheus.grid_workers.GridAnalyzer`.
 
-    Inherits from ``GridWorker``.
+    Post-processes the test results of a grid of experiments and gather them in a csv file.
 
-    TODO: complete doc
+    This csv file will gather the training statistics (seeds, accuracies, terminal conditions,...), \
+    the validation statistics and the test statistics.
+
+    Inherits from :py:class:`miprometheus.grid_workers.GridWorker`.
+
     """
 
     def __init__(self, name="GridAnalyzer"):
         """
-        Constructor for the ``GridAnalyzer``:
+        Constructor for the :py:class:`miprometheus.grid_workers.GridAnalyzer`:
 
-            - Calls basic constructor of ''GridWorker''
+            - Calls basic constructor of :py:class:`miprometheus.grid_workers.GridWorker`
 
         :param name: Name of the worker (DEFAULT: "GridAnalyzer").
         :type name: str
 
         """
         # call base constructor
-        super(GridAnalyzer, self).__init__(name=name,use_gpu=False)
+        super(GridAnalyzer, self).__init__(name=name, use_gpu=False)
 
-    def check_if_file_exists(self, dir_, filename_):
-        """ Function if file in directory exists
+    @staticmethod
+    def check_if_file_exists(dir_, filename_):
+        """
+        Checks if ``filename_`` exists in ``dir_``.
 
         :param dir_: Path to file.
+        :type dir_: str
+
         :param filename_: Name of the file to be opened and analysed.
+        :type filename_: str
+
+        :return: True if the file exists in the directory, else False
 
         """
         return os.path.isfile(os.path.join(dir_, filename_))
 
     def check_file_content(self, dir_, filename_):
-        """ Function checks if the number of lines in the file is > 1.
+        """
+        Checks if the number of lines in the file is > 1.
 
         :param dir_: Path to file.
-        :param filename_: Name of the file to be opened and analysed.
+        :type dir_: str
+
+        :param filename_: Name of the file to be opened and analyzed.
+        :type filename_: str
+
+        :return: True if the number of lines in the file is strictly greater than one.
 
         """
         return self.get_lines_number(os.path.join(dir_, filename_)) > 1
 
-    def get_lines_number(self, filename_):
-        """ Function gets number of lines in a given file.
+    @staticmethod
+    def get_lines_number(filename_):
+        """
+        Returns the number of lines in ``filename_``.
 
-        :param filename_: Name of the file (with path) to be opened and analysed.
+        :param filename_: Filepath to be opened and line-read.
+        :type filename_: str
+
+        :return: Number of lines in the file.
 
         """
         with open(filename_) as f:
-            return sum(1 for line in f)
+            return sum(1 for _ in f)
 
     def get_experiment_tests(self, experiment_path_):
-        """ Returns a list of folders with valid experiment tests.
+        """
+        Returns a list of folders containing valid test experiments data:
+
+            - A configuration (`testing_configuration.yaml`),
+            - A csv file containing a data point for the aggregated statistics (`testing_set_agg_statistics.csv`)
+
         
         :param experiment_path_: Path to experiment (training) folder.
+        :type experiment_path_: str
+
+        :return: A list of valid test experiment folders.
 
         """
         experiments_tests = []
@@ -96,27 +125,29 @@ class GridAnalyzer(GridWorker):
             for name in dirs:
                 experiments_tests.append(os.path.join(root, name))
 
-        # Keep only the folders that contain `testing.csv`.
+        # Keep only the folders that contain a test configuration file and a csv statistics file.
         experiments_tests = [elem for elem in experiments_tests if 
-            self.check_if_file_exists(elem, 'testing_configuration.yaml') and
-            self.check_if_file_exists(elem, 'testing_set_agg_statistics.csv') ]
+                             self.check_if_file_exists(elem, 'testing_configuration.yaml') and
+                             self.check_if_file_exists(elem, 'testing_set_agg_statistics.csv')]
 
-        # Check if the files contain any collected training/validation statistics.
+        # Check if the csv file contains at least one data point.
         experiments_tests = [elem for elem in experiments_tests if 
-            self.check_file_content(elem, 'testing_set_agg_statistics.csv')]
+                             self.check_file_content(elem, 'testing_set_agg_statistics.csv')]
+
         return experiments_tests
 
     def setup_grid_experiment(self):
         """
         Setups the overall experiment:
 
-        - Parses arguments and sets logger level.
+            - Parses arguments and sets logger level,
+            - Checks the presence of experiments folder,
+            - Recursively traverses the experiment folders, cherry-picking subfolders containing:
 
-        - Checks the presence of experiments folder.
+                - (a) 'training_configuration.yaml' (training configuration file),
+                - (b) 'models/model_best.pt' (checkpoint of the best saved model).
 
-        - Recursively traverses the experiment folders, cherry-picking subfolders containing \
-        (a) 'training_configuration.yaml' and (b) 'models/model_best.pt'.
-       
+
         """
         # Parse arguments.
         self.flags, self.unparsed = self.parser.parse_known_args()
@@ -129,7 +160,7 @@ class GridAnalyzer(GridWorker):
             print('Please pass the experiments directory as --expdir')
             exit(-1)
 
-        # Get experiment directory.        
+        # Get experiment directory.
         self.experiment_rootdir = self.flags.expdir
 
         # Get all sub-directories paths in expdir.
@@ -142,20 +173,15 @@ class GridAnalyzer(GridWorker):
         # Keep only the folders that contain training_configuration.yaml, training_statistics.csv and
         # training.csv and model (which contains aggregated validation statistics).
         self.experiments_list = [elem for elem in self.experiments_list if 
-            self.check_if_file_exists(elem, 'training_configuration.yaml') and
-        #    self.check_if_file_exists(elem, 'training_statistics.csv') and
-            self.check_if_file_exists(elem, 'models/model_best.pt')]
-
-        # Check if the training statistics file contain any records.
-        #self.experiments_list = [elem for elem in self.experiments_list if 
-        #     self.check_file_content(elem, 'training_statistics.csv')]
+                                 self.check_if_file_exists(elem, 'training_configuration.yaml') and
+                                 self.check_if_file_exists(elem, 'models/model_best.pt')]
 
         # Check if there are some valid folders.
         if len(self.experiments_list) == 0:
-            self.logger.error("There are no valid experiments in {} directory!".format(self.experiment_rootdir))
+            self.logger.error("There are no valid experiment folders in {} directory!".format(self.experiment_rootdir))
             exit(-2)
 
-        # List folders with "valid" experiments.
+        # List folders with "valid" experiment data.
         exp_str = "Found the following valid experiments in directory: {} \n".format(self.experiment_rootdir)
         exp_str += '='*80 + '\n'
         for exp in self.experiments_list:
@@ -172,39 +198,42 @@ class GridAnalyzer(GridWorker):
 
     def run_experiment(self, experiment_path: str):
         """
-        
-        Collects statistics for a given training validation. \
-        Analyzes whether given training experiment folder contains subfolder with tests.
+        Collects the training / validation / test statistics for a given experiment path.
 
-         - Loades and parses training configuration file.
+        Analyzes whether the given training experiment folder contains subfolders with test experiments data:
 
-         - Loads checkpoint with model and training and validation statistics.
+            - Loads and parses training configuration file,
+            - Loads checkpoint with model and training and validation statistics,
+            - Recursively traverses subdirectories looking for test experiments,
 
-       ..note::
+              .. note::
 
-            We require that test csv files are valid, i.e. contain at least one line with \
-            collected statistics (excluding the header).
+                We require that the test statistics csv files are valid, i.e. contain at least one line with \
+                collected statistics (excluding the header).
 
-        - Recursivelly traverses subdirectories looking for test experiments.
 
-        - Collects statistics from training, validation (from model checkpoint) and test experiments \
-        (from test csv files found in subdirectories).
+            - Collects statistics from training, validation (from model checkpoint) and test experiments \
+              (from test csv files found in subdirectories).
 
         :param experiment_path: Path to an experiment folder containing a training statistics.
         :type experiment_path: str
 
-        :returns: four dictionaries containing: status info (model, problem etc.), \
-        training, validation, testing.
+        :return: Four dictionaries containing:
+                    - Status info (model, problem etc.),
+                    - Training statistics,
+                    - Validation statistics,
+                    - Test statistics.
+
 
         """
-        self.logger.info('Analysing experiment from: {}'.format(experiment_path))
+        self.logger.info('Analyzing experiments from: {}'.format(experiment_path))
 
         # Create dictionaries.
         status_dict = dict()
         train_dict = dict()
         valid_dict = dict()
 
-        # Load yaml file. To get model name, problem name and random seeds
+        # Load yaml file, to get model name, problem name and random seeds.
         with open(os.path.join(experiment_path, 'training_configuration.yaml'), 'r') as yaml_file:
             params = yaml.load(yaml_file)
 
@@ -214,7 +243,7 @@ class GridAnalyzer(GridWorker):
 
         # Load checkpoint from model file.
         chkpt = torch.load(os.path.join(experiment_path, 'models/model_best.pt'),
-            map_location=lambda storage, loc: storage)
+                           map_location=lambda storage, loc: storage)
 
         status_dict['model_timestamp'] = '{0:%Y%m%d_%H%M%S}'.format(chkpt['timestamp']) 
         status_dict['train_status'] = chkpt['status']
@@ -228,13 +257,13 @@ class GridAnalyzer(GridWorker):
         train_dict['train_seed_torch'] = params['training']['seed_torch']
         train_dict['train_seed_numpy'] = params['training']['seed_numpy']                    
 
-        # Copy training statistics and add 'valid_' prefices from 
+        # Copy the training statistics from the checkpoint and add the 'train_' prefix.
         for key, value in chkpt['training_stats'].items():
             train_dict['train_{}'.format(key)] = value
         # Create "empty" equivalent.
         train_dict_empty = dict.fromkeys(train_dict.keys(), ' ')
 
-        # Copy validation statistics and add 'valid_' prefices from 
+        # Copy the validation statistics from the checkpoint and add the 'valid_' prefix.
         for key, value in chkpt['validation_stats'].items():
             valid_dict['valid_{}'.format(key)] = value
         # Create "empty" equivalent.
@@ -244,20 +273,24 @@ class GridAnalyzer(GridWorker):
         experiments_tests = self.get_experiment_tests(experiment_path)
 
         list_test_dicts = []
+
         if len(experiments_tests) > 0:
             self.logger.info('  - Found {} test(s)'.format(len(experiments_tests)))
-            # "Expand" status, train and valid dicts by empty ones.
-            list_status_dicts = [status_dict, *[status_dict_empty for i in range(len(experiments_tests)-1)]]
-            list_train_dicts = [train_dict, *[train_dict_empty for i in range(len(experiments_tests)-1)]]
-            list_valid_dicts = [valid_dict, *[valid_dict_empty for i in range(len(experiments_tests)-1)]]
+
+            # "Expand" status, train and valid dicts by empty ones, prop. to the number of test folders.
+            list_status_dicts = [status_dict, *[status_dict_empty for _ in range(len(experiments_tests) - 1)]]
+            list_train_dicts = [train_dict, *[train_dict_empty for _ in range(len(experiments_tests) - 1)]]
+            list_valid_dicts = [valid_dict, *[valid_dict_empty for _ in range(len(experiments_tests) - 1)]]
 
             # Get tests statistics.
             for experiment_test_path in experiments_tests:
                 self.logger.info('  - Analyzing test from: {}'.format(experiment_test_path))
-                # Create test dict,
+
+                # Create test dict:
                 test_dict = dict()
                 test_dict['test_config'] = os.path.join(experiment_test_path, 'testing_set_agg_statistics.yaml')
                 test_dict['test_start'] = os.path.basename(os.path.normpath(experiment_test_path))[5:]
+
                 # Load yaml file and get random seeds.
                 with open(os.path.join(experiment_test_path, 'testing_configuration.yaml'), 'r') as yaml_file:
                     test_params = yaml.load(yaml_file)  
@@ -265,55 +298,72 @@ class GridAnalyzer(GridWorker):
                     test_dict['test_seed_torch'] = test_params['testing']['seed_torch']
                     test_dict['test_seed_numpy'] = test_params['testing']['seed_numpy']                    
 
+                # Load csv file and copy test statistics
                 with open(os.path.join(experiment_test_path, 'testing_statistics.csv'), mode='r') as f:
                     # Open file.
                     test_reader = csv.DictReader(f)
+
                     # Copy training statistics.
                     for row in test_reader:
                         for key, value in row.items():
                             test_dict['test_{}'.format(key)] = value   
+
                 list_test_dicts.append(test_dict)
+
         else:
             self.logger.info('  - Could not find any valid tests')
             list_status_dicts = [status_dict]
             list_train_dicts = [train_dict]
             list_valid_dicts = [valid_dict]
+
             # Add "empty test entry"
             list_test_dicts.append({})
 
         # Return all dictionaries with lists
         return list_status_dicts, list_train_dicts, list_valid_dicts, list_test_dicts
 
-    def merge_list_dicts(self, list_dicts):
+    @staticmethod
+    def merge_list_dicts(list_dicts):
         """
-        Function merges list of ditionaries by using filling the missing fields with spaces.
+        Merges a list of dictionaries by filling the missing fields with spaces into one dict.
         
-        :param list_dicts: List of dictionaries, potentially containing different headers, that will be merged.
+        :param list_dicts: List of dictionaries, potentially containing different headers, which will be merged.
+        :type list_dicts: list
+
+        :return: dict, resulting of the merge.
+
         """
-        # Create "unified" header.
+        # Create a "unified" header.
         header = set(k for d in list_dicts for k in d)
-        # Create "empty" dict with unified header.
-        empty_dict = {k:' ' for k in header}
+
+        # Create an "empty" dict from the unified header.
+        empty_dict = {k: ' ' for k in header}
+
         # "Fill" all lists with empty gaps.
         list_filled_dicts = []
-        for i,_ in enumerate(list_dicts):
+        for i, _ in enumerate(list_dicts):
             list_filled_dicts.append({**empty_dict, **(list_dicts[i])})
-        # Zip lists of dics.
+
+        # Zip lists of dicts.
         final_dict = dict(zip(header, zip(*[d.values() for d in list_filled_dicts])))
+
         # Return the result.
         return final_dict
 
     def run_grid_experiment(self):
         """
-        For each experiment path from the self.experiments_lists collects four lists of dictionaries, \
-        merges all them together and saves result to a single csv file.
+        Collects four list of dicts from each experiment path contained in ``self.experiments_lists``.
+
+        Merges all them together and saves result to a single csv file.
+
         """
         try:
-            # Go throught experiments one by one and collect data.
+            # Go through the experiments one by one and collect data.
             list_statuses = []
             list_trains = []
             list_valids = []
             list_tests = []
+
             for exp in self.experiments_list:
                 statuses, trains, valids, tests = self.run_experiment(exp)
                 list_statuses.extend(statuses)
@@ -328,25 +378,27 @@ class GridAnalyzer(GridWorker):
             tests = self.merge_list_dicts(list_tests)
 
             # Merge everything into one big dictionary..
-            exp_values =  {**statuses, **trains, **valids, **tests}
+            exp_values = {**statuses, **trains, **valids, **tests}
 
             # create results file
-            results_file = os.path.join(self.experiment_rootdir, "{0:%Y%m%d_%H%M%S}_grid_analysis.csv".format(datetime.now()))
+            results_file = os.path.join(self.experiment_rootdir,
+                                        "{0:%Y%m%d_%H%M%S}_grid_analysis.csv".format(datetime.now()))
 
             with open(results_file, "w") as outfile:
                 writer = csv.writer(outfile, delimiter=',')
                 writer.writerow(exp_values.keys())
                 writer.writerows(zip(*exp_values.values()))
 
-            self.logger.info('Analyzis finished')
-            self.logger.info('Results stored in {}'.format(results_file))
+            self.logger.info('Analysis finished')
+            self.logger.info('Results stored in {}.'.format(results_file))
+
         except KeyboardInterrupt:
-            self.logger.info('Grid analyzis interrupted!')
+            self.logger.info('Grid analysis interrupted!')
 
 
 def main():
     """
-    Entry point function for the ``GridAnalyzer``.
+    Entry point function for the :py:class:`miprometheus.grid_workers.GridAnalyzer`.
 
     """
     grid_analyzer = GridAnalyzer()
