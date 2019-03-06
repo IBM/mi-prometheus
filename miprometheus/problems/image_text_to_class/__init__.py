49c185a74336b8bb6bdec430bd12dd0467c8ee27
Chaitanya Shivade <43888804+cshivade@users.noreply.github.com>
2019-03-06 15:30:36 -0800

    Added VQAMED as a problem

--- miprometheus/problems/image_text_to_class/__init__.py
+++ miprometheus/problems/image_text_to_class/__init__.py
@@ -2,5 +2,6 @@ from .clevr import CLEVR
 from .image_text_to_class_problem import ObjectRepresentation, ImageTextToClassProblem
 from .sort_of_clevr import SortOfCLEVR
 from .shape_color_query import ShapeColorQuery
+from .vqa_med_2019 import VQAMED
 
-__all__ = ['CLEVR', 'ObjectRepresentation', 'ImageTextToClassProblem', 'SortOfCLEVR', 'ShapeColorQuery']
+__all__ = ['CLEVR', 'ObjectRepresentation', 'ImageTextToClassProblem', 'SortOfCLEVR', 'ShapeColorQuery', 'VQAMED']
