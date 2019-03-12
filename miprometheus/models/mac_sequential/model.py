efe16cb3482d2cd7dfab09b380dc7e48b342caa0
Vincent Albouy <vincent.albouy@ibm.com>
2019-03-12 14:43:22 -0700

    Update model.py

--- miprometheus/models/mac_sequential/model.py
+++ miprometheus/models/mac_sequential/model.py
@@ -280,7 +280,7 @@ class MACNetworkSequential(Model):
             logits_pointing[:,i,:] = self.output_unit_pointing(memory, h)
 
 
-        return logits, logits_pointing
+        return logits.cuda(), logits_pointing.cuda()
 
     @staticmethod
     def generate_figure_layout():
