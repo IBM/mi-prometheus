29ebd7429d3d7a46bc42ff761d86127f6c8e3c11
tkornut <tkornut@us.ibm.com>
2019-04-02 13:47:56 -0700

    safe_yaml fix

--- miprometheus/utils/param_interface.py
+++ miprometheus/utils/param_interface.py
@@ -280,7 +280,7 @@ class ParamInterface(Mapping):
         # Open file and try to add that to list of parameter dictionaries.
         with open(yaml_path, 'r') as stream:
             # Load parameters.
-            params_from_yaml = yaml.load(stream)
+            params_from_yaml = yaml.safe_load(stream)
 
         # add config param
         self.add_config_params(params_from_yaml)
