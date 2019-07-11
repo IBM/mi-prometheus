82e532b6539bef2b79e3339e1607ffc57ac1bdf2
T.S. Jayram <t.s.jayram@gmail.com>
2019-07-10 19:28:19 -0700

    Implementing RNN

--- miprometheus/problems/seq_to_seq/algorithmic/functional/parity_cl.py
+++ miprometheus/problems/seq_to_seq/algorithmic/functional/parity_cl.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__author__ = "Tomasz Kornuta"
+__author__ = "T.S. Jayram"
 
 import torch
 import numpy as np
