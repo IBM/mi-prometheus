b9c47ae69e639370ace4bb7ee23c8d0cdfb9b2bc
Jayram Thathachar <jayram@us.ibm.com>
2019-08-13 14:51:23 -0700

    Attention module check

--- miprometheus/models/VWM_model/interaction_module.py
+++ miprometheus/models/VWM_model/interaction_module.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# MIT License
-#
-# Copyright (c) 2018 Kim Seonghyeon
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# ------------------------------------------------------------------------------
-#
 # Copyright (C) IBM Corporation 2018
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -58,7 +34,7 @@ class InteractionModule(Module):
     def __init__(self, dim):
         """
         Constructor for the ``InteractionModule``.
-        :param dim: global 'd' hidden dimension
+        :param dim: dimension of feature vectors
         :type dim: int
         """
 
@@ -79,10 +55,7 @@ class InteractionModule(Module):
         Forward pass of the ``InteractionModule``.
 
         :param base_object: query [batch_size x dim]
-        :type base_object: torch.tensor
-
         :param  feature_objects: [batch_size x num_objects x dim]
-        :type feature_objects: torch.tensor
 
         :return: feature_objects_modified [batch_size x num_objects x dim]
         """
