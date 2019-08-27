536baef226d72fb75407fc3080d7168a87c35064
Jayram Thathachar <jayram@us.ibm.com>
2019-08-26 22:45:01 -0700

    Different reasoning unit

--- miprometheus/models/vwm_model/reasoning_unit.py
+++ miprometheus/models/vwm_model/reasoning_unit.py
@@ -79,7 +79,8 @@ class ReasoningUnit(Module):
         t_none = temporal_class_weights[:, 3]
 
         # check if temporal context is last or latest
-        temporal_test_1 = torch.relu(t_last + t_latest - t_now - t_none)
+        # temporal_test_1 = torch.relu(t_last + t_latest - t_now - t_none)
+        temporal_test_1 = (t_last + t_latest) * (1 - t_now - t_none)
 
         # conditioned on temporal context,
         # check if we should replace existing memory object
@@ -90,17 +91,15 @@ class ReasoningUnit(Module):
         do_add_new = (1 - valid_mo) * valid_vo * temporal_test_1
 
         # check if temporal context is now or latest
-        temporal_test_2 = torch.relu(t_now + t_latest - t_last - t_none)
+        temporal_test_2 = (t_now + t_latest) * (1 - t_last - t_none)
 
         # conditioned on temporal context, check if we have a valid visual object
         image_match = valid_vo * temporal_test_2
 
         # check if temporal context is either last, or latest but there is no visual object
-
-        temporal_test_3 = torch.relu(t_last + t_latest * (1 - valid_vo) - t_now - t_none)
+        temporal_test_3 = (t_last + t_latest * (1 - valid_vo)) * (1 - t_now - t_none)
 
         # conditioned on temporal context, check if we have a valid memory object
-
         memory_match = valid_mo * temporal_test_3
 
         return image_match, memory_match, do_replace, do_add_new
